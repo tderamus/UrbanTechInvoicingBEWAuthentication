51d895e5162e1ea58bb74a64e010428c2212d883
tderamus <deramust@gmail.com>
2025-05-19 19:56:59 -0500

    Added initial setup files, setup directory structure and added models to the project

---
+++ UrbanTechInvoicing/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrbanTechInvoicing.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; }
+        public required string Name { get; set; }
+        public required string EmailAddress { get; set; }
+        public required string PhoneNumber { get; set; }
+    }
+}
---
+++ UrbanTechInvoicing/Models/Service.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrbanTechInvoicing.Models
+{
+    public class Service
+    {
+        [Key]
+        public Guid ServiceId { get; set; }
+        public required string ServiceName { get; set; }
+        public required string Description { get; set; }
+    }
+}
